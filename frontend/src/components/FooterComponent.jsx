6318d64ab4daabbb7ac4bff83623de99c43c6d4a
Omar <54633759+RamoBENOT@users.noreply.github.com>
2020-12-07 13:53:58 +0100

    FrontEnd

---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class FooterComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+        <div class="p-3 mb-2 bg-primary text-white">All Rights Reserved 2020</div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default FooterComponent;
